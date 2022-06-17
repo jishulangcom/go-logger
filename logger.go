27309b7fd832aef491852ab40277fe60e2d09cf8
jishulangcom <57217780+jishulangcom@users.noreply.github.com>
2022-06-17 11:01:48 +0800

    trace

--- logger.go
+++ logger.go
@@ -1,6 +1,10 @@
 package logger
 
 import (
+	"context"
+	"fmt"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
@@ -46,54 +50,127 @@ func Debug(args ...interface{}) {
 	logger.Debug(args...)
 }
 
+func DebugTrace(ctx context.Context, args ...interface{}) {
+	Trace(ctx).Debug(args...)
+}
+
 func Debugf(template string, args ...interface{}) {
 	logger.Debugf(template, args...)
+}
+
+func DebugfTrace(ctx context.Context, template string, args ...interface{}) {
+	Trace(ctx).Debugf(template, args...)
 }
 
 func Info(args ...interface{}) {
 	logger.Info(args...)
 }
 
+func InfoTrace(ctx context.Context, args ...interface{}) {
+	Trace(ctx).Info(args...)
+}
+
 func Infof(template string, args ...interface{}) {
 	logger.Infof(template, args...)
+}
+
+func InfofTrace(ctx context.Context, template string, args ...interface{}) {
+	Trace(ctx).Infof(template, args...)
 }
 
 func Warn(args ...interface{}) {
 	logger.Warn(args...)
 }
 
+func WarnTrace(ctx context.Context, args ...interface{}) {
+	Trace(ctx).Warn(args...)
+}
+
 func Warnf(template string, args ...interface{}) {
 	logger.Warnf(template, args...)
+}
+
+func WarnfTrace(ctx context.Context, template string, args ...interface{}) {
+	Trace(ctx).Warnf(template, args...)
 }
 
 func Error(args ...interface{}) {
 	logger.Error(args...)
 }
 
+func ErrorTrace(ctx context.Context, args ...interface{}) {
+	Trace(ctx).Error(args...)
+}
+
 func Errorf(template string, args ...interface{}) {
 	logger.Errorf(template, args...)
+}
+
+func ErrorfTrace(ctx context.Context, template string, args ...interface{}) {
+	Trace(ctx).Errorf(template, args...)
 }
 
 func DPanic(args ...interface{}) {
 	logger.DPanic(args...)
 }
 
+func DPanicTrace(ctx context.Context, args ...interface{}) {
+	Trace(ctx).DPanic(args...)
+}
+
 func DPanicf(template string, args ...interface{}) {
 	logger.DPanicf(template, args...)
+}
+
+func DPanicfTrace(ctx context.Context, template string, args ...interface{}) {
+	Trace(ctx).DPanicf(template, args...)
 }
 
 func Panic(args ...interface{}) {
 	logger.Panic(args...)
 }
 
+func PanicTrace(ctx context.Context, args ...interface{}) {
+	Trace(ctx).Panic(args...)
+}
+
 func Panicf(template string, args ...interface{}) {
 	logger.Panicf(template, args...)
+}
+
+func PanicfTrace(ctx context.Context, template string, args ...interface{}) {
+	Trace(ctx).Panicf(template, args...)
 }
 
 func Fatal(args ...interface{}) {
 	logger.Fatal(args...)
 }
 
+func FatalTrace(ctx context.Context, args ...interface{}) {
+	Trace(ctx).Fatal(args...)
+}
+
 func Fatalf(template string, args ...interface{}) {
 	logger.Fatalf(template, args...)
+}
+
+func FatalfTrace(ctx context.Context, template string, args ...interface{}) {
+	Trace(ctx).Fatalf(template, args...)
+}
+
+// logger带上TraceId
+func Trace(ctx context.Context) *zap.SugaredLogger {
+	var jTraceId jaeger.TraceID
+	if parent := opentracing.SpanFromContext(ctx); parent != nil {
+		parentCtx := parent.Context()
+		if tracer := opentracing.GlobalTracer(); tracer != nil {
+			mySpan := tracer.StartSpan("my info", opentracing.ChildOf(parentCtx))
+			if sc, ok := mySpan.Context().(jaeger.SpanContext); ok {
+				jTraceId = sc.TraceID()
+			}
+			defer mySpan.Finish()
+		}
+	}
+
+	return logger.With(zap.String(jaeger.TraceContextHeaderName, fmt.Sprint(jTraceId)))
 }
